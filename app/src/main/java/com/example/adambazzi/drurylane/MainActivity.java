05fb8060467a4a807a7eb2d8b8a735a339e5f3a5
Adam Bazzi <adamjamil.bazzi64@stclairconnect.ca>
2017-12-14 14:41:04 -0500

    Added datatype class Created the Fragment that will contain the menu and the add to cart button

--- app/src/main/java/com/example/adambazzi/drurylane/MainActivity.java
+++ app/src/main/java/com/example/adambazzi/drurylane/MainActivity.java
@@ -18,7 +18,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
-                    MainFragment.OnFragmentInteractionListener{
+                    MainFragment.OnFragmentInteractionListener,
+                    premadeMenu.OnFragmentInteractionListener{
     FragmentManager fm;
 
     @Override
@@ -36,14 +37,14 @@ public class MainActivity extends AppCompatActivity
             transaction.commit();
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
