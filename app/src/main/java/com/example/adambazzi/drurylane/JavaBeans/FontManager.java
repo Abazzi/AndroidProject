806e9ad0698826e8912b3c4e492d327b0c85e270
Adam Bazzi <adamjamil.bazzi64@stclairconnect.ca>
2018-01-16 22:35:28 -0500

    Added Font Awesome

---
+++ app/src/main/java/com/example/adambazzi/drurylane/JavaBeans/FontManager.java
@@ -0,0 +1,20 @@
+package com.example.adambazzi.drurylane.JavaBeans;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by Adam on 1/16/2018.
+ */
+
+public class FontManager {
+
+    public static final String ROOT = "fonts/",
+            FONTAWESOME = ROOT + "fontawesome-webfont.ttf";
+
+    public static Typeface getTypeface(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+
+}
+
